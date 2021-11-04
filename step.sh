105f7cc829b6a28ffe90a8274a8036ac0c04bc04
Imran Mentese <imran.mentese@arhs-spikeseed.com>
2021-11-04 12:07:07 +0100

    fix

--- step.sh
+++ step.sh
@@ -22,8 +22,8 @@ echo "Left icon: ${left_icon}"
 echo "Right icon: ${right_icon}"
 echo "Output path: ${output_path}"
 
-python3 "$(dirname $0)/generator.py3 "$source_image" "$left_icon" "$right_icon" "$output_path" || error_exit "Python file not found."
+python3 "$(dirname $0)/generator.py3" "$source_image" "$left_icon" "$right_icon" "$output_path" || error_exit "Python file not found."
 
 zip "overlayed_images.zip" "$output_path"
-cp "overlayed_images.zip" "$BITRISE_DEPLOY_DIR/overlayed_images.zip" || true
+cp "overlayed_images.zip" "$BITRISE_DEPLOY_DIR/overlayed_images.zip"
 exit 0
